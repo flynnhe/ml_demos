fd16c2d888a971b49eee9d27528022c4260665a3
= <flynnhe@gmail.com>
2016-03-19 17:09:49 +0000

    ml_demos: added l2 weight decay

--- js/neuralnetwork.js
+++ js/neuralnetwork.js
@@ -1,14 +1,15 @@
 // START OF NEURALNETWORK
-function NeuralNetwork(numHiddenLayers, numUnitsPerLayer, activateFn) {
+function NeuralNetwork(lrRate, numHiddenLayers, numUnitsPerLayer, activateFn) {
   this.numHiddenLayers = numHiddenLayers;
   this.numUnitsPerLayer = numUnitsPerLayer;
   this.activateFn = activateFn;
-  this.lr_rate = 0.01;
+  this.lrRate = lrRate;
   this.momentum = 0.9;
+  this.weightDecay = 0.001;
   this.currEpoch = 1;
   this.weights = new Array(numHiddenLayers + 1);
   this.avgError = 0.0;
-  numInputUnits = 1;
+  numInputUnits = 4;
   numOutputUnits = 1;
 
   // set up the layers (currently only supporting same number of neurons in each hidden layer)
@@ -39,17 +40,23 @@ function NeuralNetwork(numHiddenLayers, numUnitsPerLayer, activateFn) {
       for (var j = 0; j < nTo; j++) {
         if (j == 0) {
           this.weights[k][i].push(0); // don't have a weight going from bias to bias
-        } else {
-          this.weights[k][i].push(Math.random());
+        }
+        else {
+          this.weights[k][i].push(Math.random() * 2 - 1);
         }
         deltasW[k][i].push(0);
       }
     }
   }
 
+  // set the base learning rate
+  this.setLrRate = function(r) {
+    this.lrRate = r;
+  }
+
   // get the network's prediction for a given x
   this.predict = function(x) {
-    tmp_outputs[0] = [1, x]; // add 1 for bias
+    tmp_outputs[0] = [1, x, x*x, x*x*x, x*x*x*x]; // add 1 for bias
     var data = forwardPropagate(x, this.weights, tmp_outputs, x, this.activateFn);
     return tmp_outputs[numHiddenLayers + 1][1];
   }
@@ -57,12 +64,19 @@ function NeuralNetwork(numHiddenLayers, numUnitsPerLayer, activateFn) {
   // train the network to minimize the L2 Euclidean loss
   this.trainOneEpoch = function(examples) {
     var numExamples = examples.length;
+    var indices = new Array(examples.length);
+    for (var i = 0; i < examples.length; i++) {
+      indices[i] = i;
+    }
     // do forward pass through whole training set
+    // randomize the training examples
+    var order = shuffle(indices);
     this.avgError = 0.0;
-    for (var n = 0; n < numExamples; n++) {
+    for (var idx = 0; idx < numExamples; idx++) {
+      var n = order[idx];
       input = examples[n].x;
-      target = examples[n].y;
-      outputs[0] = [1, input];
+      target = examples[n].y; 
+      outputs[0] = [1, input, input*input, input*input*input, input*input*input*input];
       var data = forwardPropagate(input, this.weights, outputs, target, this.activateFn);
       deltasData[numHiddenLayers + 1] = data[1];
 
@@ -75,7 +89,7 @@ function NeuralNetwork(numHiddenLayers, numUnitsPerLayer, activateFn) {
         for (var i = 1; i < nFrom; i++) {
           deltasData[k][i] = 0;
           for (var j = 1; j < nTo; j++) {
-            deltasData[k][i] += this.weights[k][i][j] * deltasData[k + 1][j];
+            deltasData[k][i] += this.weights[k][i][j] * deltasData[k+1][j];
           }
           if (k != 0) { // input layer is not activated
             deltasData[k][i] *= dActivate(outputs[k][i], this.activateFn);
@@ -89,25 +103,24 @@ function NeuralNetwork(numHiddenLayers, numUnitsPerLayer, activateFn) {
         var nTo = nodeCounts[k + 1] + 1;
         for (var j = 1; j < nTo; j++) {
           // bias updates
-          deltasW[k][0][j] = (this.lr_rate / Math.pow(10, k)) * deltasData[k +
-            1][j] + this.momentum * deltasW[k][0][j];
+          deltasW[k][0][j] = (this.lrRate / Math.pow(2, k)) * deltasData[k+1][j] + this.momentum * deltasW[k][0][j];
           this.weights[k][0][j] -= deltasW[k][0][j];
           for (var i = 1; i < nFrom; i++) {
             var dEdW = 0;
             if (k != 0) {
-              dEdW = activate(outputs[k][i], this.activateFn) * deltasData[k +
-                1][j];
-            } else {
-              dEdW = outputs[k][i] * deltasData[k + 1][j];
+              dEdW = activate(outputs[k][i], this.activateFn) * deltasData[k+1][j];
+              dEdW += this.weightDecay * this.weights[k][i][j];
             }
-            deltasW[k][i][j] = (this.lr_rate / Math.pow(10, k)) * dEdW + this
-              .momentum * deltasW[k][i][j];
+            else {
+              dEdW = outputs[k][i] * deltasData[k+1][j];
+              dEdW += this.weightDecay * this.weights[k][i][j];
+            }
+            deltasW[k][i][j] = (this.lrRate / Math.pow(2, k)) * dEdW + this.momentum * deltasW[k][i][j];
             this.weights[k][i][j] -= deltasW[k][i][j];
           }
         }
       }
-      this.avgError += (this.predict(examples[n].x) - target) * (this.predict(
-        examples[n].x) - target);
+      this.avgError += (this.predict(examples[n].x) - target) * (this.predict(examples[n].x) - target);
     }
     this.avgError /= numExamples;
     this.currEpoch++;
@@ -119,44 +132,55 @@ function NeuralNetwork(numHiddenLayers, numUnitsPerLayer, activateFn) {
 // normalize points to have zero mean and unit variance
 function normalize(points) {
   var n = points.length;
-  var mean = [0, 0];
-  var std = [0, 0];
+  var mean = [0, 0, 0, 0, 0];
+  var std = [0, 0, 0, 0, 0];
   for (var i = 0; i < n; i++) { // compute mean
     mean[0] += points[i].x;
-    mean[1] += points[i].y;
+    mean[1] += points[i].x2;
+    mean[2] += points[i].x3;
+    mean[3] += points[i].x4;
+    mean[4] += points[i].y;
   }
-  mean = [mean[0] / n, mean[1] / n];
+  mean = [mean[0]/n, mean[1]/n, mean[2]/n, mean[3]/n, mean[4]/n];
 
   for (var i = 0; i < n; i++) { // compute std
     std[0] += (points[i].x - mean[0]) * (points[i].x - mean[0]);
-    std[1] += (points[i].y - mean[1]) * (points[i].y - mean[1]);
+    std[1] += (points[i].x2 - mean[1]) * (points[i].x2 - mean[1]);
+    std[2] += (points[i].x3 - mean[2]) * (points[i].x3 - mean[2]);
+    std[3] += (points[i].x4 - mean[3]) * (points[i].x4 - mean[3]);
+    std[4] += (points[i].y - mean[4]) * (points[i].y - mean[4]);  
   }
-  std = [Math.sqrt(std[0] / n), Math.sqrt(std[1] / n)];
+  std = [Math.sqrt(std[0]/n),Math.sqrt(std[1]/n),Math.sqrt(std[2]/n),Math.sqrt(std[3]/n),Math.sqrt(std[4]/n)];
 
   // subtract mean and divide by std
   for (var i = 0; i < n; i++) {
     points[i].x = (points[i].x - mean[0]) / std[0];
-    points[i].y = (points[i].y - mean[1]) / std[1];
+    points[i].x2 = (points[i].x2 - mean[1]) / std[1];
+    points[i].x3 = (points[i].x3 - mean[2]) / std[2];
+    points[i].x4 = (points[i].x4 - mean[3]) / std[3];
+    points[i].y = (points[i].y - mean[4]) / std[4];
   }
 
   return points;
 }
 
-function forwardPropagate(inputs, weights, outputs, targets, activateFn) {
+function forwardPropagate(inputs, weights, outputs, targets, activateFn)
+{
   var numLayers = weights.length;
   var numHiddenLayers = numLayers - 1;
   var sum = 0;
 
   for (var i = 1; i <= numLayers; i++) {
     if (i == 1) {
-      sum = computeSum(weights[i - 1], [1, inputs]);
-    } else {
-      sum = computeSum(weights[i - 1], activate(outputs[i - 1], activateFn));
+      sum = computeSum(weights[i-1], [1, inputs, inputs*inputs, inputs*inputs*inputs, inputs*inputs*inputs*inputs]);
+    }
+    else {
+      sum = computeSum(weights[i-1], activate(outputs[i-1], activateFn));
     }
     outputs[i] = sum;
   }
 
-  var errorData = computeError(targets, outputs[numHiddenLayers + 1]);
+  var errorData = computeError(targets, outputs[numHiddenLayers + 1], weights);
   return errorData;
 }
 
@@ -174,18 +198,33 @@ function computeSum(weights, inputs) {
   return sum;
 }
 
-function computeError(target, output) {
+function computeError(target, output, weights) {
   target1 = [output[0], target]; // add term to match bias
   var numUnits = target1.length;
   var deltasO = new Array(numUnits);
   var error = 0;
   deltasO[0] = 0;
   for (var j = 1; j < numUnits; j++) {
-    error += (output[j] - target1[j]) * (output[j] - target1[j]);
+    error += dActivate(output[j], activateFn) * (output[j] - target1[j]) * (output[j] - target1[j]);
+    error += l2norm(weights);
     deltasO[j] = output[j] - target1[j];
   }
   error *= 0.5;
   return [error, deltasO];
+}
+
+function l2norm(weights) {
+  var norm = 0.0;
+  for (var i = 0; i < weights.length; i++) {
+    var nFrom = weights[i].length;
+    var nTo = weights[i][0].length; 
+    for (var j = 0; j < nFrom; j++) {
+			for (var k = 0; k < nTo; k++) {
+				norm += weights[i][j][k] * weights[i][j][k];
+			}
+    }
+  }
+  return Math.sqrt(norm);
 }
 
 function activate(x, activateFn) {
@@ -270,7 +309,7 @@ function dTanh(y) {
   if (typeof y == 'number') y = [y];
   var dy = new Array(y.length);
   for (var i = 0; i < y.length; i++) {
-    dy[i] = 1 - (tanh(y[i])) * (tanh(y[i]));
+    dy[i] = 1 - (tanh(y[i]))*(tanh(y[i]));
   }
   return dy;
 }
@@ -280,10 +319,30 @@ function dRelu(y) {
   var dy = new Array(y.length);
   for (var i = 0; i < y.length; i++) {
     if (y[i] > 0) {
-      dy[i] = 1;
+        dy[i] = 1;
     } else {
-      dy[i] = 0;
+        dy[i] = 0;
     }
   }
   return dy;
+}
+
+function shuffle(array) {
+  var copy = [], n = array.length, i;
+
+  // While there remain elements to shuffle…
+  while (n) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * array.length);
+
+    // If not already shuffled, move it to the new array.
+    if (i in array) {
+      copy.push(array[i]);
+      delete array[i];
+      n--;
+    }
+  }
+
+  return copy;
 }
